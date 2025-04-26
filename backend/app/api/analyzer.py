181738ba2c0811062b98723af32cfbe4c0a1f651
Finley Sullivan <finleysullivan@Finleys-MacBook-Pro.local>
2025-04-26 15:18:30 -0400

    Fixed squat detection bug by analyzing squat at minimum vertical height

--- backend/app/api/analyzer.py
+++ backend/app/api/analyzer.py
@@ -5,7 +5,7 @@ import numpy as np
 def analyze_video(video_path: str, side: str = "right") -> dict:
 
     # Depth thresholds
-    GOOD_DEPTH_THRESHOLD = 0.015    # hip clearly below knee
+    GOOD_DEPTH_THRESHOLD = 0.002    # hip clearly below knee
     BORDERLINE_THRESHOLD = 0.0  # about parallel
 
     # Determine which side we are analyzing from
@@ -36,20 +36,18 @@ def analyze_video(video_path: str, side: str = "right") -> dict:
             rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results = pose.process(rgb)
         
-            # grab x, y coordinates for hip (24), knee (26), and ankle (28)
-            # and calcuate the knee angle from the right side
+            # grab x, y coordinates for hip, knee, and ankle
             if results.pose_landmarks:
                 lm = results.pose_landmarks.landmark
                 hip = [lm[hip_idx].x, lm[hip_idx].y]
                 knee = [lm[knee_idx].x, lm[knee_idx].y]
                 ankle = [lm[ankle_idx].x, lm[ankle_idx].y]
 
-                # position based depth check
+                # append hip_y, knee_y, angle triplets in an array
                 angle = calculate_angle(hip, knee, ankle)
-                diff = hip[1] - knee[1] # hip and knee y coords
-                frame_data.append((angle, diff))
+                frame_data.append((hip[1], knee[1], angle))
 
-                print(f"Frame {frame_count} | hip_y={hip[1]:.4f} | knee_y={knee[1]:.4f} | diff={diff:.4f}")
+                print(f"Frame {frame_count} | hip_y={hip[1]:.4f} | knee_y={knee[1]:.4f}")
 
 
         frame_count += 1
@@ -60,10 +58,27 @@ def analyze_video(video_path: str, side: str = "right") -> dict:
     if not frame_data:
         return {"feedback": "No pose detected."}
     
-    # Analyze bottom 5 frames (smallest knee angle)
-    frame_data.sort(key=lambda x: x[0])
+    # sort the data by lowest hip_y, and take bottom 5 frames
+    frame_data.sort(key=lambda x : x[0])
     bottom_frames = frame_data[:5]
-    depth_value = sum(diff for _, diff in bottom_frames) / len(bottom_frames)
+
+    # calculate hip-knee difference for each bottom frame
+    diffs = []
+    for hip_y, knee_y, _ in bottom_frames:
+        diff = hip_y - knee_y
+        diffs.append(diff)
+    
+    # if less than 3 valid frames, too shallow
+    if len(diffs) < 3:
+        return {
+            "depth_value": None,
+            "side": side,
+            "feedback": "Too shallow, not enough frames to analyze"
+        }
+
+    # average top 3 biggest diffs
+    selected_diffs = sorted(diffs, reverse=True)[:3]
+    depth_value = sum(selected_diffs) / len(selected_diffs) 
     
     # give feedback based on the average depth values
     if depth_value > GOOD_DEPTH_THRESHOLD:
